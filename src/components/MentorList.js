4a5679b22c300ca8b2cf1972c774a5f613ccd42c
Asfo <asfoone@gmail.com>
2019-10-06 20:17:18 -0500

    New card styles

--- src/components/MentorList.js
+++ src/components/MentorList.js
@@ -4,19 +4,14 @@ import { Link } from 'react-router-dom';
 class MentorList extends Component {
     render() {
         return (
-            <div className="col-sm-4 productList">
-                <Link to={{ pathname: '/MentorList', state: { id: this.props.data } }}>
-                    <div className="productItems">
-                        <div>
-                            {/* <img className="card-img-top"
-                            src={`../assets/${this.props.data.image}`} alt="Card cap" /> */}
-                            <img className="card-img-top"
-                                src={this.props.data.image} alt="Card cap" />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title font-weight-bold">{this.props.data.name}</h5>
-                            <p>{this.props.data.technology}</p>
-                            <p>{this.props.data.country}</p>
+            <div className="col-sm-4">
+                <Link to={{ pathname: '/MentorList', state: { id: this.props.data } }} className="card">
+                    <div className="thumbnail" style={ { backgroundImage: `url(${this.props.data.image})` } } alt="Card cap" />
+                    <div className="content-card">
+                        <h3>{this.props.data.name}</h3>
+                        <p className="tags">{this.props.data.technology}</p>
+                        <div className="bottom-info">
+                          <p>{this.props.data.country}</p>
                         </div>
                     </div>
                 </Link>
