9a683fd79c6dc9fe6c549c2a61ffe34ec10a92e7
v.martynenko <v.martynenko@semrush.com>
2019-10-12 16:14:34 +0300

    add mentor

--- src/response/response.js
+++ src/response/response.js
@@ -88,7 +88,14 @@ const data = [
         image: 'https://github.com/MrMrRobat',
         technology: 'Python, async.io',
         country: 'Russia',
-        biography: 'Make staff since 2000. Love cakes and beer.' // 'I am Full Stack Web Developer'
+        biography: 'Make staff since 2000. Love cakes and beer.'
+    },
+    {
+        name: 'Martyn Son',
+        image: 'https://github.com/martyns0n',
+        technology: 'Javascript, React, Eslint/Stylelint',
+        country: 'Russia',
+        biography: 'Three years experience in web development'
     }
 ];
 
