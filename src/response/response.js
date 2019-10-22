a55c8b6010ab2dc659e2acef36e8840d3b797425
Elangovan <6679438+elangovanshanthi@users.noreply.github.com>
2019-10-22 13:33:14 +0530

    Update response.js

--- src/response/response.js
+++ src/response/response.js
@@ -15,20 +15,6 @@ const data = [
     biography: "I am Full Stack Web Developer and a ML enthusiast"
   },
   {
-    name: "Arseny Boykov",
-    image: "https://github.com/MrMrRobat",
-    technology: "Python, async.io",
-    country: "Russia",
-    biography: "Make staff since 2000. Love cakes and beer."
-  },
-  {
-    name: "Martyn Son",
-    image: "https://github.com/martyns0n",
-    technology: "Javascript, React, Eslint/Stylelint",
-    country: "Russia",
-    biography: "Three years experience in web development"
-  },
-  {
     name: "Tanay",
     image: "https://avatars2.githubusercontent.com/u/10216863?s=400&v=4",
     technology: "Javascript",
@@ -188,6 +174,20 @@ const data = [
     technology: 'C#, JS, PHP',
     country: 'SÃO PAULO - BRAZIL',
     biography: 'I am Full Stack Web Developer'
+  },
+  {
+    name: "Arseny Boykov",
+    image: "https://github.com/MrMrRobat",
+    technology: "Python, async.io",
+    country: "Russia",
+    biography: "Make staff since 2000. Love cakes and beer."
+  },
+  {
+    name: "Martyn Son",
+    image: "https://github.com/martyns0n",
+    technology: "Javascript, React, Eslint/Stylelint",
+    country: "Russia",
+    biography: "Three years experience in web development"
   }
 ];
 
