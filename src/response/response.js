43244ecb52912578041ae07f2d335348648660cf
Elangovan <6679438+elangovanshanthi@users.noreply.github.com>
2019-11-01 19:46:00 +0530

    Updated the mentor data

--- src/response/response.js
+++ src/response/response.js
@@ -1,369 +1,281 @@
 const data = [
-  {
-    id: 1,
-    name: "Elangovan",
-    image: "https://via.placeholder.com/300/",
-    technology: "ReactJs, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 2,
-    name: "Darpan",
-    image:
-      "https://avatars0.githubusercontent.com/u/14301996?s=400&u=71e9200f5037c290c092f01c3ec408e5705e4cca&v=4",
-    technology: "Java, Nodejs, AngularJS, ReactJS, Spring",
-    country: "India",
-    biography: "I am Full Stack Web Developer and a ML enthusiast"
-  },
-  {
-    id: 3,
-    name: "Arseny Boykov",
-    image: "https://github.com/MrMrRobat",
-    technology: "Python, async.io",
-    country: "Russia",
-    biography: "Make staff since 2000. Love cakes and beer."
-  },
-  {
-    id: 4,
-    name: "Martyn Son",
-    image: "https://github.com/martyns0n",
-    technology: "Javascript, React, Eslint/Stylelint",
-    country: "Russia",
-    biography: "Three years experience in web development"
-  },
-  {
-    id: 5,
-    name: "Tanay",
-    image: "https://avatars2.githubusercontent.com/u/10216863?s=400&v=4",
-    technology: "Javascript",
-    country: "India",
-    biography: "Frontend Developer"
-  },
-  {
-    id: 6,
-    name: "Krish",
-    image: "https://via.placeholder.com/300/",
-    technology: "Java, ReactJs, Javascript",
-    country: "United States",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 7,
-    name: "Rashid",
-    image: "https://avatars2.githubusercontent.com/u/1191631",
-    technology: "Javascript, Angular, ReactJS, NodeJS",
-    country: "India",
-    biography: "I am a Frontend Engineer"
-  },
-  {
-    id: 8,
-    name: "Gurpreet Singh",
-    image: "https://avatars2.githubusercontent.com/u/8161329",
-    technology: "Javascript, ReactJS",
-    country: "Germany",
-    biography: "Lead Front End Developer"
-  },
-  {
-    id: 9,
-    name: "Atul Krishna",
-    image: "https://avatars3.githubusercontent.com/u/11033984",
-    technology: "Javascript, ReactJs, Angular, Python, Java",
-    country: "India",
-    biography: "I am Software Engineer based in Bengaluru, India."
-  },
-  {
-    id: 10,
-    name: "Mayank Kumar",
-    image: "https://avatars0.githubusercontent.com/u/27151470?s=100&v=4",
-    technology: "Java, Javascript",
-    country: "India",
-    biography: "Android Developer."
-  },
-  {
-    id: 11,
-    name: "Will",
-    image: "https://avatars0.githubusercontent.com/u/10842135",
-    technology: "Javascript, Java",
-    country: "USA",
-    biography:
-      "I am Sr Project Manager for the largest entertainment company in the world!"
-  },
-  {
-    id: 12,
-    name: "Weje Emmauel",
-    image:
-      "https://s.gravatar.com/avatar/82880ef540f92d552e36a26bf79ca9df?s=80",
-    technology: "PHP(Laravel), Ruby, React JS",
-    country: "Ireland",
-    biography: "I'm a software engineer"
-  },
-  {
-    id: 13,
-    name: "Matt",
-    image: "https://avatars2.githubusercontent.com/u/15995932",
-    technology: "Javascript, ReactJS, NodeJS",
-    country: "USA",
-    biography:
-      "I am a university professor with head in the cloud dreams of achieving Financial Independence through Full Stack Web Development."
-  },
-  {
-    id: 14,
-    name: "Saliban",
-    image: "https://via.placeholder.com/300/",
-    technology: "Javascript,React.js",
-    country: "Iran",
-    biography: "I am a FrontEnd developer"
-  },
-  {
-    id: 15,
-    name: "John",
-    image: "https://via.placeholder.com/300/",
-    technology: "Php, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 16,
-    name: "Gabriel",
-    image: "https://avatars0.githubusercontent.com/u/56734377?s=40&v=4",
-    technology: "Python",
-    country: "United Kingdom",
-    biography: "I am study my Master in Computer Science"
-  },
-  {
-    id:17,
-    name: "Mayank",
-    image: "https://avatars1.githubusercontent.com/u/23161291?s=100&v=4",
-    technology: "ReactJs, Java, Python",
-    country: "India",
-    biography: "I am working as a backend-developer."
-  },
-  {
-    id: 18,
-    name: "Naresh",
-    image: "https://via.placeholder.com/300/",
-    technology: "AngularJS, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 19,
-    name: "Abdul",
-    image: "https://via.placeholder.com/300/",
-    technology: "Angularjs, Python",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:20,
-    name: "Elangovan",
-    image: "https://via.placeholder.com/300/",
-    technology: "ReactJs, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:21,
-    name: "Krish",
-    image: "https://via.placeholder.com/300/",
-    technology: "Java, ReactJs, Javascript",
-    country: "United States",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:22,
-    name: "John",
-    image: "https://via.placeholder.com/300/",
-    technology: "Php, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:23,
-    name: "Naresh",
-    image: "https://via.placeholder.com/300/",
-    technology: "AngularJS, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 24,
-    name: "Abdul",
-    image: "https://via.placeholder.com/300/",
-    technology: "Angularjs, Python",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 25,
-    name: "Haidar Rifki",
-    image: "https://avatars3.githubusercontent.com/u/19185336",
-    technology: "Javascript, NodeJS, PHP",
-    country: "Indonesia",
-    biography:
-      "I am Software Engineer based in Surakarta, Central Java, Indonesia."
-  },
-  {
-    id: 26,
-    name: "Diego Rondão",
-    image: "https://avatars1.githubusercontent.com/u/13041373",
-    technology: "C#, JS, PHP",
-    country: "SÃO PAULO - BRAZIL",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id: 27,
-    name: "Arseny Boykov",
-    image: "https://github.com/MrMrRobat",
-    technology: "Python, async.io",
-    country: "Russia",
-    biography: "Make staff since 2000. Love cakes and beer."
-  },
-  {
-    id: 28,
-    name: "Martyn Son",
-    image: "https://github.com/martyns0n",
-    technology: "Javascript, React, Eslint/Stylelint",
-    country: "Russia",
-    biography: "Three years experience in web development"
-  },
-  {
-    id: 29,
-    name: "Meriem Zaid",
-    image: "https://github.com/iMeriem",
-    technology: "JavaScript, Python",
-    country: "Morocco",
-    biography: "I write code for living"
-  },
-  {
-    id: 30,
-    name: "Abdellah Ramadan",
-    image: "https://github.com/giantthinker",
-    technology: "Laravel, Reactjs, Angular, PHP",
-    country: "Cameroon",
-    biography: "Software development as a career"
-  },
-  {
-    id:31,
-    name: "Krish",
-    image: "https://via.placeholder.com/300/",
-    technology: "Java, ReactJs, Javascript",
-    country: "United States",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:32,
-    name: "John",
-    image: "https://via.placeholder.com/300/",
-    technology: "Php, Javascript",
-    country: "India",
-    biography: "I am Full Stack Web Developer"
-  },
-  {
-    id:33,
-    name: 'Rashid',
-    image: 'https://avatars2.githubusercontent.com/u/1191631',
-    technology: 'Javascript, Angular, ReactJS, NodeJS',
-    country: 'India',
-    biography: 'I am a Frontend Engineer'
-  },
-  {
-    id:34,
-    name: 'Gurpreet Singh',
-    image: 'https://avatars2.githubusercontent.com/u/8161329',
-    technology: 'Javascript, ReactJS',
-    country: 'Germany',
-    biography: 'Lead Front End Developer'
-  },
-  {
-    id:35,
-    name: 'Atul Krishna',
-    image: 'https://avatars3.githubusercontent.com/u/11033984',
-    technology: 'Javascript, ReactJs, Angular, Python, Java',
-    country: 'India',
-    biography: 'I am Software Engineer based in Bengaluru, India.'
-  },
-  {
-    id:36,
-    name: "Mayank Kumar",
-    image: "https://avatars0.githubusercontent.com/u/27151470?s=100&v=4",
-    technology: "Java, Javascript",
-    country: "India",
-    biography: "Android Developer."
-  },
-  {
-    id: 37,
-    name: 'Will',
-    image: 'https://avatars0.githubusercontent.com/u/10842135',
-    technology: 'Javascript, Java, ',
-    country: 'USA',
-    biography: 'I am Sr Project Manager for the largest entertainment company in the world!'
-  },
-  {
-    id: 38,
-    name: 'Leanne Lacey',
-    image: 'https://github.com/Leannelb',
-    technology: 'JavaScript, Angular, HTML, CSS, SASS, NodeJS',  //'Php, Javascript',
-    country: 'Ireland', //india
-    biography: 'I\'m a Developer from green \'ol Ireland. Working mainly with Angular 2'
-  },
-  {
-    id: 39,
-    name: "Waing La Min Lwin",
-    image: "https://github.com/archonn",
-    technology: "Nodejs, React, Python",
-    country: "Myanmar",
-    biography: "Software developer focusing on building ML-powered products"
-  },
-  {
-    id: 40,
-    name: "Rafidah Affendi",
-    image: "https://github.com/r4fidah",
-    technology: "HTML, JavaScript, Python",
-    country: "Malaysia",
-    biography: "Self-taught Data Scientist"
-  },{
-
-    id:41,
-    name: 'Weje Emmauel',
-    image: 'https://s.gravatar.com/avatar/82880ef540f92d552e36a26bf79ca9df?s=80',
-    technology: 'PHP(Laravel), Ruby, React JS',
-    country: 'Ireland',
-    biography: 'I\'m a software engineer'
-   },
-   {
-    id:42,
-    name: 'Matt',
-    image: 'https://avatars2.githubusercontent.com/u/15995932',
-    technology: 'Javascript, ReactJS, NodeJS',
-    country: 'USA',
-    biography: 'I am a university professor with head in the cloud dreams of achieving Financial Independence through Full Stack Web Development.'
-  },
-  {
-    id:43,
-    name: 'Haidar Rifki',
-    image: 'https://avatars3.githubusercontent.com/u/19185336',
-    technology: 'Javascript, NodeJS, PHP',
-    country: 'Indonesia',
-    biography: 'I am Software Engineer based in Surakarta, Central Java, Indonesia.'
-  },
-  {
-    id:44,
-    name: 'Diego Rondão',
-    image: 'https://avatars1.githubusercontent.com/u/13041373',
-    technology: 'C#, JS, PHP',
-    country: 'SÃO PAULO - BRAZIL',
-    biography: 'I am Full Stack Web Developer'
-  },
-  {
-    id:45,
-    name: "Alandouglas",
-    image: "https://avatars0.githubusercontent.com/u/42749979?s=400&u=43129c15c770f7c6c4e473e2e92b0339052a019b&v=4",
-    technology: "PHP, Javascript, HTML, CSS, Python"
-    country: "Brazil",
-    biography: "I am  a jr full-stack dev in formation"
-  },
+    {
+        id: 1,
+        name: "Elangovan",
+        image: "https://via.placeholder.com/300/",
+        technology: "ReactJs, Javascript",
+        country: "India",
+        biography: "I am Full Stack Web Developer"
+    },
+    {
+        id: 2,
+        name: "Darpan",
+        image:
+        "https://avatars0.githubusercontent.com/u/14301996?s=400&u=71e9200f5037c290c092f01c3ec408e5705e4cca&v=4",
+        technology: "Java, Nodejs, AngularJS, ReactJS, Spring",
+        country: "India",
+        biography: "I am Full Stack Web Developer and a ML enthusiast"
+    },
+    {
+        id: 3,
+        name: "Arseny Boykov",
+        image: "https://github.com/MrMrRobat",
+        technology: "Python, async.io",
+        country: "Russia",
+        biography: "Make staff since 2000. Love cakes and beer."
+    },
+    {
+        id: 4,
+        name: "Martyn Son",
+        image: "https://github.com/martyns0n",
+        technology: "Javascript, React, Eslint/Stylelint",
+        country: "Russia",
+        biography: "Three years experience in web development"
+    },
+    {
+        id: 5,
+        name: "Tanay",
+        image: "https://avatars2.githubusercontent.com/u/10216863?s=400&v=4",
+        technology: "Javascript",
+        country: "India",
+        biography: "Frontend Developer"
+    },
+    {
+        id: 6,
+        name: "Krish",
+        image: "https://via.placeholder.com/300/",
+        technology: "Java, ReactJs, Javascript",
+        country: "United States",
+        biography: "I am Full Stack Web Developer"
+    },
+    {
+        id: 7,
+        name: "Rashid",
+        image: "https://avatars2.githubusercontent.com/u/1191631",
+        technology: "Javascript, Angular, ReactJS, NodeJS",
+        country: "India",
+        biography: "I am a Frontend Engineer"
+    },
+    {
+        id: 8,
+        name: "Gurpreet Singh",
+        image: "https://avatars2.githubusercontent.com/u/8161329",
+        technology: "Javascript, ReactJS",
+        country: "Germany",
+        biography: "Lead Front End Developer"
+    },
+    {
+        id: 9,
+        name: "Atul Krishna",
+        image: "https://avatars3.githubusercontent.com/u/11033984",
+        technology: "Javascript, ReactJs, Angular, Python, Java",
+        country: "India",
+        biography: "I am Software Engineer based in Bengaluru, India."
+    },
+    {
+        id: 10,
+        name: "Mayank Kumar",
+        image: "https://avatars0.githubusercontent.com/u/27151470?s=100&v=4",
+        technology: "Java, Javascript",
+        country: "India",
+        biography: "Android Developer."
+    },
+    {
+        id: 11,
+        name: "Will",
+        image: "https://avatars0.githubusercontent.com/u/10842135",
+        technology: "Javascript, Java",
+        country: "USA",
+        biography:
+        "I am Sr Project Manager for the largest entertainment company in the world!"
+    },
+    {
+        id: 12,
+        name: "Weje Emmauel",
+        image:
+        "https://s.gravatar.com/avatar/82880ef540f92d552e36a26bf79ca9df?s=80",
+        technology: "PHP(Laravel), Ruby, React JS",
+        country: "Ireland",
+        biography: "I'm a software engineer"
+    },
+    {
+        id: 13,
+        name: "Matt",
+        image: "https://avatars2.githubusercontent.com/u/15995932",
+        technology: "Javascript, ReactJS, NodeJS",
+        country: "USA",
+        biography:
+        "I am a university professor with head in the cloud dreams of achieving Financial Independence through Full Stack Web Development."
+    },
+    {
+        id: 14,
+        name: "Saliban",
+        image: "https://via.placeholder.com/300/",
+        technology: "Javascript,React.js",
+        country: "Iran",
+        biography: "I am a FrontEnd developer"
+    },
+    {
+        id: 15,
+        name: "John",
+        image: "https://via.placeholder.com/300/",
+        technology: "Php, Javascript",
+        country: "India",
+        biography: "I am Full Stack Web Developer"
+    },
+    {
+        id: 16,
+        name: "Gabriel",
+        image: "https://avatars0.githubusercontent.com/u/56734377?s=40&v=4",
+        technology: "Python",
+        country: "United Kingdom",
+        biography: "I am study my Master in Computer Science"
+    },
+    {
+        id:17,
+        name: "Mayank",
+        image: "https://avatars1.githubusercontent.com/u/23161291?s=100&v=4",
+        technology: "ReactJs, Java, Python",
+        country: "India",
+        biography: "I am working as a backend-developer."
+    },
+    {
+        id: 18,
+        name: "Naresh",
+        image: "https://via.placeholder.com/300/",
+        technology: "AngularJS, Javascript",
+        country: "India",
+        biography: "I am Full Stack Web Developer"
+    },
+    {
+        id: 19,
+        name: "Abdul",
+        image: "https://via.placeholder.com/300/",
+        technology: "Angularjs, Python",
+        country: "India",
+        biography: "I am Full Stack Web Developer"
+    },
+    {
+        id: 20,
+        name: "Haidar Rifki",
+        image: "https://avatars3.githubusercontent.com/u/19185336",
+        technology: "Javascript, NodeJS, PHP",
+        country: "Indonesia",
+        biography:
+        "I am Software Engineer based in Surakarta, Central Java, Indonesia."
+    },
+    {
+        id: 21,
+        name: "Diego Rondão",
+        image: "https://avatars1.githubusercontent.com/u/13041373",
+        technology: "C#, JS, PHP",
+        country: "SÃO PAULO - BRAZIL",
+        biography: "I am Full Stack Web Developer"
+    },  
+    {
+        id: 22,
+        name: "Meriem Zaid",
+        image: "https://github.com/iMeriem",
+        technology: "JavaScript, Python",
+        country: "Morocco",
+        biography: "I write code for living"
+    },
+    {
+        id: 23,
+        name: "Abdellah Ramadan",
+        image: "https://github.com/giantthinker",
+        technology: "Laravel, Reactjs, Angular, PHP",
+        country: "Cameroon",
+        biography: "Software development as a career"
+    },
+    {
+        id: 24,
+        name: 'Leanne Lacey',
+        image: 'https://github.com/Leannelb',
+        technology: 'JavaScript, Angular, HTML, CSS, SASS, NodeJS',  //'Php, Javascript',
+        country: 'Ireland', //india
+        biography: 'I\'m a Developer from green \'ol Ireland. Working mainly with Angular 2'
+    },
+    {
+        id: 25,
+        name: "Waing La Min Lwin",
+        image: "https://github.com/archonn",
+        technology: "Nodejs, React, Python",
+        country: "Myanmar",
+        biography: "Software developer focusing on building ML-powered products"
+    },
+    {
+        id: 26,
+        name: "Rafidah Affendi",
+        image: "https://github.com/r4fidah",
+        technology: "HTML, JavaScript, Python",
+        country: "Malaysia",
+        biography: "Self-taught Data Scientist"
+    }, 
+    {
+        id:27,
+        name: "Alandouglas",
+        image: "https://avatars0.githubusercontent.com/u/42749979?s=400&u=43129c15c770f7c6c4e473e2e92b0339052a019b&v=4",
+        technology: "PHP, Javascript, HTML, CSS, Python"
+        country: "Brazil",
+        biography: "I am  a jr full-stack dev in formation"
+    },
+    {
+        id:28,
+        name: "Caleb",
+        image: "https://avatars3.githubusercontent.com/u/17759183",
+        technology: "Javascript, Reactjs, Java, Ruby",
+        country: "USA",
+        biography: "Junior software developer"
+    },
+    {
+        id:29,
+        name: 'Kay Ogunbowale',
+        image: 'https://avatars0.githubusercontent.com/u/21027632',
+        technology: 'Nodejs, Android, Java, Javascript',
+        country: 'Nigeria',
+        biography: 'Passionate software developer'
+    },
+    {
+        id:30,
+        name: "Razvan Sofian",
+        image: "https://github.com/Rufusnu",
+        technology: "Python, C++, C#, GDScript",
+        country: "Romania",
+        biography: "I am a full time student at computer science in Cluj, Romania"
+    },
+    {
+        id:31,
+        name: "Manjeet Singh",
+        image: "https://avatars2.githubusercontent.com/u/14962072?s=460&v=4",
+        technology: "Python, JavaScript, HTML",
+        country: "India",
+        biography: "Web Developer @cityexploro Technical Scripter @geeksforgeeks Technology Enthusiast and a Sports Programmer."
+    },
+    {
+        id:32,
+        name: "Arshad Kazmi",
+        image: "https://avatars2.githubusercontent.com/u/4654382?s=460&v=4",
+        technology: "JavaScript, Python",
+        country: "India",
+        biography: "Let's remove the `lib` directory ➿"
+    },
+    {
+        id:33,
+        name: "Ananda Aguiar",
+        image: "https://github.com/Anandinha",
+        technology: "Java, JavaScript, HTML, CSS",
+        country: "Brasil",
+        biography: "Computer Engineer Student"
+    },
+    {
+        id:34,
+        name: "Adarsha Meghanada",
+        image: "https://github.com/adarhp0",
+        technology: "HTML, JavaScript, NodeJS, Python",
+        country: "India",
+        biography: "Full stack Developer"
+    }
 ];
 
 export default data;
