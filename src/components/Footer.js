8f72b440478963df8deaa053ad47296b5848b5ff
Elangovan <6679438+elangovanshanthi@users.noreply.github.com>
2019-12-17 10:46:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import "../styles.css";
-
-import "../assets/githublogo.png";
-
+import '../styles.css';
+import '../assets/githublogo.png';
 
 class Footer extends Component {
-
-
     render() {
         return (
             <div className="Footer">
-
-
                 <Link to="/" >  Coding-AI </Link>
-
-
                 {/* <p>Check us out on <a>GitHub</a></p> */}
             </div>
         );
